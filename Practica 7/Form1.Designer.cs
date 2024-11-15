5c5a103d963fa8211177f3559b072f14e257f1d1
Carlos Escalante <L24580603@reynosa.tecnm.mx>
2024-11-15 09:48:38 -0600

    Add files via upload

---
+++ Practica 7/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Practica_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            valor = new TextBox();
+            Resultado = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(27, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Datos ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(52, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresa el valor:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(27, 119);
+            label3.Name = "label3";
+            label3.Size = new Size(143, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Opciones de Conversion ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(27, 256);
+            label4.Name = "label4";
+            label4.Size = new Size(124, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Resultado en Grados ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(52, 289);
+            label5.Name = "label5";
+            label5.Size = new Size(137, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Equivalente en Grados: ";
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(32, 153);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 38);
+            button1.TabIndex = 5;
+            button1.Text = "FAHRENHEIT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(147, 153);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 38);
+            button2.TabIndex = 6;
+            button2.Text = "CENTIGRADOS";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.Location = new Point(97, 197);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 37);
+            button3.TabIndex = 7;
+            button3.Text = "BORRAR";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // valor
+            // 
+            valor.Location = new Point(147, 55);
+            valor.Name = "valor";
+            valor.Size = new Size(121, 23);
+            valor.TabIndex = 8;
+            // 
+            // Resultado
+            // 
+            Resultado.Location = new Point(58, 324);
+            Resultado.Name = "Resultado";
+            Resultado.Size = new Size(108, 23);
+            Resultado.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGreen;
+            ClientSize = new Size(301, 377);
+            Controls.Add(Resultado);
+            Controls.Add(valor);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Conversiones ";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox valor;
+        private TextBox Resultado;
+    }
+}
